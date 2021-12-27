0271cd5d6c8bccdeb3adbf806960b145029a1eef
Rob Challen <rob@terminological.co.uk>
2021-12-27 18:06:24 +0000

    jepidemic stable

--- src/main/java/uk/co/terminological/rjava/LogController.java
+++ src/main/java/uk/co/terminological/rjava/LogController.java
@@ -2,7 +2,14 @@ package uk.co.terminological.rjava;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
+import java.io.IOException;
 import java.io.PrintStream;
+import java.net.URISyntaxException;
+import java.net.URL;
+import java.sql.Date;
+import java.text.DateFormat;
+import java.util.jar.JarFile;
+import java.util.zip.ZipEntry;
 
 import org.apache.logging.log4j.Level;
 import org.apache.logging.log4j.LogManager;
@@ -91,5 +98,37 @@ public class LogController {
 		builder.add(rootLogger);
 		Configurator.initialize(builder.build());
 		Configurator.setAllLevels(LogManager.getRootLogger().getName(),lev);
+	}
+	
+	/**
+	 * Handles files, jar entries, and deployed jar entries in a zip file (EAR).
+	 * @return The date if it can be determined, or null if not.
+	 */
+	public static String getClassBuildTime() {
+	    Date d = null;
+	    Class<?> currentClass = new Object() {}.getClass().getEnclosingClass();
+	    URL resource = currentClass.getResource(currentClass.getSimpleName() + ".class");
+	    if (resource != null) {
+	        if (resource.getProtocol().equals("file")) {
+	            try {
+	                d = new Date(new File(resource.toURI()).lastModified());
+	            } catch (URISyntaxException ignored) { }
+	        } else if (resource.getProtocol().equals("jar")) {
+	            String path = resource.getPath();
+	            d = new Date( new File(path.substring(5, path.indexOf("!"))).lastModified() );    
+	        } else if (resource.getProtocol().equals("zip")) {
+	            String path = resource.getPath();
+	            File jarFileOnDisk = new File(path.substring(0, path.indexOf("!")));
+	            //long jfodLastModifiedLong = jarFileOnDisk.lastModified ();
+	            //Date jfodLasModifiedDate = new Date(jfodLastModifiedLong);
+	            try(JarFile jf = new JarFile (jarFileOnDisk)) {
+	                ZipEntry ze = jf.getEntry (path.substring(path.indexOf("!") + 2));//Skip the ! and the /
+	                long zeTimeLong = ze.getTime ();
+	                Date zeTimeDate = new Date(zeTimeLong);
+	                d = zeTimeDate;
+	            } catch (IOException|RuntimeException ignored) { }
+	        }
+	    }
+	    return d == null ? "unknown" : DateFormat.getInstance().format(d);
 	}
 }
