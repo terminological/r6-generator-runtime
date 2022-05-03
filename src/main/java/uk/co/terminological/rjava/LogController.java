7399b79ac07b5c9e4e5d8ea7ec2fa90e1b462e32
Rob Challen <rob@terminological.co.uk>
2022-05-03 13:09:23 +0100

    minor bug fixes

--- src/main/java/uk/co/terminological/rjava/LogController.java
+++ src/main/java/uk/co/terminological/rjava/LogController.java
@@ -6,8 +6,10 @@ import java.io.IOException;
 import java.io.PrintStream;
 import java.net.URISyntaxException;
 import java.net.URL;
-import java.sql.Date;
-import java.text.DateFormat;
+import java.time.Instant;
+import java.time.LocalDateTime;
+import java.time.ZoneId;
+import java.time.format.DateTimeFormatter;
 import java.util.jar.JarFile;
 import java.util.zip.ZipEntry;
 
@@ -105,17 +107,20 @@ public class LogController {
 	 * @return The date if it can be determined, or null if not.
 	 */
 	public static String getClassBuildTime() {
-	    Date d = null;
+	    LocalDateTime d = null;
 	    Class<?> currentClass = new Object() {}.getClass().getEnclosingClass();
 	    URL resource = currentClass.getResource(currentClass.getSimpleName() + ".class");
 	    if (resource != null) {
 	        if (resource.getProtocol().equals("file")) {
 	            try {
-	                d = new Date(new File(resource.toURI()).lastModified());
+	                d = Instant.ofEpochMilli(new File(resource.toURI()).lastModified())
+	                		.atZone(ZoneId.systemDefault()).toLocalDateTime();
 	            } catch (URISyntaxException ignored) { }
 	        } else if (resource.getProtocol().equals("jar")) {
 	            String path = resource.getPath();
-	            d = new Date( new File(path.substring(5, path.indexOf("!"))).lastModified() );    
+	            d = Instant.ofEpochMilli(
+	            		new File(path.substring(5, path.indexOf("!"))).lastModified()
+	            	).atZone(ZoneId.systemDefault()).toLocalDateTime();
 	        } else if (resource.getProtocol().equals("zip")) {
 	            String path = resource.getPath();
 	            File jarFileOnDisk = new File(path.substring(0, path.indexOf("!")));
@@ -124,11 +129,12 @@ public class LogController {
 	            try(JarFile jf = new JarFile (jarFileOnDisk)) {
 	                ZipEntry ze = jf.getEntry (path.substring(path.indexOf("!") + 2));//Skip the ! and the /
 	                long zeTimeLong = ze.getTime ();
-	                Date zeTimeDate = new Date(zeTimeLong);
-	                d = zeTimeDate;
+	                d = Instant.ofEpochMilli(
+		            		zeTimeLong
+	    	            	).atZone(ZoneId.systemDefault()).toLocalDateTime();
 	            } catch (IOException|RuntimeException ignored) { }
 	        }
 	    }
-	    return d == null ? "unknown" : DateFormat.getInstance().format(d);
+	    return d == null ? "unknown" : d.format(DateTimeFormatter.ISO_DATE_TIME);
 	}
 }
